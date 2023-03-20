8b8a83ab657e65008f71c34834439914f2c01f25
samueller9 <60405236+samueller9@users.noreply.github.com>
2023-03-20 16:32:20 -0700

    Initial Commit

---
+++ Habitual/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Habitual
+//
+//  Created by Stefan Mueller on 3/20/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
